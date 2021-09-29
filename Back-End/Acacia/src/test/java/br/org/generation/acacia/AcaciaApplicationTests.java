1e795e6e314d50b264d302e882e8f3c268307aef
Guzius <gustavotaques11@gmail.com>
2021-09-29 10:17:02 -0300

    Criando projeto

---
+++ Back-End/Acacia/src/test/java/br/org/generation/acacia/AcaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.acacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
