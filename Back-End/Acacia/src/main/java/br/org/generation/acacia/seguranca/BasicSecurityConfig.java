fb0ed4bb0a20530ae590fa44426204bfb6bc58e6
jessica lemos gomes <lemosgomes24@gmail.com>
2021-10-18 09:47:45 -0300

    atualizando senha

--- Back-End/Acacia/src/main/java/br/org/generation/acacia/seguranca/BasicSecurityConfig.java
+++ Back-End/Acacia/src/main/java/br/org/generation/acacia/seguranca/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package br.org.generation.acacia.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,23 +20,33 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
+		
+        auth.userDetailsService(userDetailsService);
+		
+		auth.inMemoryAuthentication()
+		.withUser("root")
+		.password(passowordEncoder().encode("root"))
+		.authorities("ROLE_USER");
 	
+	}
+
 	@Bean
 	public PasswordEncoder passowordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
+	protected  void  configure(HttpSecurity  http) throws  Exception {
+
+	http.authorizeRequests()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	    .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
 	}
+
 }
