c09600876ad44ed1b712099295d9e2e7df0c8123
unknown <vitor.santana03@hotmail.com>
2021-10-29 16:23:19 -0300

    Esqueleto Task9

--- Front-End/projeto-integrador/src/App.tsx
+++ Front-End/projeto-integrador/src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/NavBar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import Sobre from './paginas/sobre/Sobre';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <Switch>
+          <div style={{minHeight: '100vh'}}>
+
+            <Route exact path='/'>
+              <Login />
+            </Route>
+
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+
+            <Route path='/sobre'>
+              <Sobre />
+            </Route>
+          </div>
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
 
--- Front-End/projeto-integrador/src/paginas/login/Login.tsx
+++ Front-End/projeto-integrador/src/paginas/login/Login.tsx
@@ -1,11 +1,66 @@
 import React from 'react'
+import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@material-ui/core';
+import { Padding } from '@mui/icons-material';
+import './Login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px' }}
+    >
+      •
+    </Box>
+  );
+  return (
+    <>
+      <Grid container direction='row' justifyContent='center' alignItems='center'>
+        <Grid item xs={6} alignItems='center'>
+          <Box paddingX={20}>
+            <Card sx={{ minWidth: 275 }} style={{ background: '#90ee90', borderRadius: '10%', padding: '10px' }}>
+              <CardContent>
+                <form>
+                  <Typography variant='h4' gutterBottom color='textPrimary' component='h4' align='center' style={{ fontWeight: 'bold', color: 'black' }}>Entrar</Typography>
+                  <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                  <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                  <Box marginTop={2} textAlign='center'>
+                    <Link to='/home' className='text-decorator-none'>
+                      <Button type='submit' variant='contained' style={{ color: 'black', background: '#4169e1' }}>
+                        Logar
+                      </Button>
+                    </Link>
+                  </Box>
+                </form>
+                <Box display='flex' justifyContent='center' marginTop={2}>
+                  <Box marginRight={1}>
+                    <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                  </Box>
+
+                  <Box marginRight={1}>
+                    <Typography variant='subtitle1' gutterBottom align='center' style={{ fontWeight: 'bold' }}>Cadastre-se</Typography>
+                  </Box>
+                </Box>
+              </CardContent>
+            </Card>
+          </Box>
+        </Grid>
+        <Grid item xs={6} style={{
+          backgroundImage: `url(https://cdn.pixabay.com/photo/2015/04/24/00/24/flowers-737061_960_720.jpg)`,
+          backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
+        }}>
+
+        </Grid>
+      </Grid>
+
+    </>
+  )
 }
 
 export default Login
+
+/*<Link to='/home' className='text-decorator-none'>
+                                <Button type='submit' variant='contained' style={{color : 'red'}}>
+                                    Logar
+                                </Button>
+                            </Link> */
