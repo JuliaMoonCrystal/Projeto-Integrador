2c75eb4354148986a1f59c185753c0bab26cc5a4
jessica lemos gomes <lemosgomes24@gmail.com>
2021-11-02 15:15:14 -0300

    forçando o conflito

--- Front-End/projeto-integrador/src/components/estaticos/navbar/NavBar.tsx
+++ Front-End/projeto-integrador/src/components/estaticos/navbar/NavBar.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
-
-function NavBar() {
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import './NavBar.css'
+function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor' >
+                        <Typography variant="h5" color="inherit">
+                            ACACIA
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Sobre Nós
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor' >
+                            <Typography variant="h6" color="inherit">
+                                Contato
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                               Login
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
-export default NavBar
+export default Navbar;
