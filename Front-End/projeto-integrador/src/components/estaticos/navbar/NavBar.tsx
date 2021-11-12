9ccd3c1acfd6b3d99ff295d4233d63eafcd5ec1a
julia <jufarias20002@gmail.com>
2021-11-12 16:03:03 -0300

    att

--- Front-End/projeto-integrador/src/components/estaticos/navbar/NavBar.tsx
+++ Front-End/projeto-integrador/src/components/estaticos/navbar/NavBar.tsx
@@ -3,8 +3,22 @@ import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link, useHistory } from "react-router-dom";
 import './NavBar.css';
 import useLocalStorage from 'react-use-localstorage';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Navbar() {
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     const [token, setToken] = useLocalStorage('token');
     let history = useHistory();
 
@@ -34,19 +48,54 @@ function Navbar() {
                         <Link to="/sobre" className="fontNav1">
                             <Box mx={1} >
                                 <Typography variant="h6">Sobre nós</Typography>
-
-
+                                
                             </Box>
                         </Link>
 
                         <Link to="/login" className="fontNav1">
-                            <Box mx={1}  onClick={Lougout}>
+                            <Box mx={1} onClick={Lougout}>
                                 <Typography variant="h6" >Logout</Typography>
 
                             </Box>
                         </Link>
-                    </Box>
+                    
+                    <Box alignItems="rigth" ml="auto" >
+                        <Button
+                            id="basic-button"
+                            aria-controls="basic-menu"
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}
+                        >
+                            <MenuIcon className="icon" />
+                        </Button>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            <MenuItem onClick={handleClose}>Minha conta</MenuItem>
 
+
+                            <Link to={"/formularioPostagem"} className='text-decorator-none'>
+                                <MenuItem onClick={handleClose}>Fazer Postagem</MenuItem>
+                            </Link>
+
+                            <Link to={"/formularioTema"} className='text-decorator-none'>
+                                <MenuItem onClick={handleClose}>Adicionar novo tema</MenuItem>
+                            </Link>
+
+                            <Link to={"/login"} className='text-decorator-none'>
+                                <MenuItem onClick={Lougout}>Logout</MenuItem>
+                            </Link>
+
+                        </Menu>
+                    </Box>
+                   </Box>
                 </Toolbar>
             </AppBar>
         </>
