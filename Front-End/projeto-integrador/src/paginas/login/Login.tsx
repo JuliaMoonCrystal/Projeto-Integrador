53e8e2d3a008a9454692e2b3168de325f7e646d7
unknown <vitor.santana03@hotmail.com>
2021-11-14 21:22:22 -0300

    Att

--- Front-End/projeto-integrador/src/paginas/login/Login.tsx
+++ Front-End/projeto-integrador/src/paginas/login/Login.tsx
@@ -51,20 +51,12 @@ function Login() {
                         <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
 
                         <Box marginTop={2} textAlign='center'>
-<<<<<<< HEAD
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Login
-                                </Button>
-
-</Box>
-=======
                             <Button type='submit' variant='contained' color='primary'>
                                 Login
                             </Button>
 
                         </Box>
 
->>>>>>> 24128cb6c07094076421a24f9b6d9e72e9528288
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
