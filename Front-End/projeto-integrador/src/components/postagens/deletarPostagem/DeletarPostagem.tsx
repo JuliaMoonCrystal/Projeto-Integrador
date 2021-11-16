c3cc70ab095d05f756ca26d7e212cf5971f3a5e9
unknown <vitor.santana03@hotmail.com>
2021-11-16 09:58:40 -0300

    Implementação dos deletes

---
+++ Front-End/projeto-integrador/src/components/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DeletarPostagem() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DeletarPostagem
---
+++ Front-End/projeto-integrador/src/components/postagens/putPostagem/PutPostagem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PutPostagem() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PutPostagem
